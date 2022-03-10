30bf955fd25b384ef4c9d4971e4b74ba77f059fe
Marzio95 <marziodellarocca14@gmail.com>
2022-03-10 15:32:17 +0100

    aggiungo prima foto di Vue

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ const app = new Vue (
         el: '#containerVs',
         data: {
             firstMessage: 'Hello World',
+            foto: 'https://picsum.photos/200/300'
         }
     }
      
